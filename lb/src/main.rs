ff02bd4727bef68e9ec1cf61e65a01be84c5f059
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2024-01-22 09:37:40 +0200

    init

---
+++ lb/src/main.rs
@@ -0,0 +1,72 @@
+use actix_cors::Cors;
+use actix_web::{middleware::Logger, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use log::info;
+use serde::{Deserialize, Serialize};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Welcome to the Load Balancer Server");
+    initiate_logging();
+
+    let server_address =
+        std::env::var("LB_SERVER_HOST").expect("Missed 'LB_SERVER_HOST' environment variable");
+    info!("Starting server at {}", server_address);
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Cors::permissive())
+            .wrap(Logger::default())
+            .service(web::scope("").service(web::resource("/").route(web::get().to(health))))
+    })
+    .bind(server_address)?
+    .run()
+    .await
+}
+
+pub fn initiate_logging() {
+    std::env::set_var("RUST_LOG", "debug, actix_web=debug");
+
+    let env = dotenv::from_filename(".env").expect("'.env' not found.");
+    dbg!(env);
+
+    if std::env::var("PWD").is_err() {
+        std::env::set_var("PWD", env!("CARGO_MANIFEST_DIR"));
+        let pwd = std::env::var("PWD").unwrap();
+        dbg!(pwd);
+    }
+
+    env_logger::init();
+}
+
+async fn health(req: HttpRequest) -> impl Responder {
+    let request_address_message = format!(
+        "Received request from {}",
+        req.connection_info().peer_addr().unwrap_or_default()
+    );
+    let request_method_message =
+        format!("{} {} {:?}", req.method(), req.uri().path(), req.version());
+    let request_host_message = format!("Host: {:?}", req.headers().get("Host").unwrap());
+    let request_user_agent_message =
+        format!("User-Agent: {:?}", req.headers().get("User-Agent").unwrap());
+    let request_accept_message = format!("Accept: {:?}", req.headers().get("Accept").unwrap());
+
+    let message: String = format!(
+        "{}\n{}\n{}\n{}\n{}\n",
+        request_address_message,
+        request_method_message,
+        request_host_message,
+        request_user_agent_message,
+        request_accept_message
+    );
+    println!("{}", message);
+    
+    // let status: String = "Ok".to_string();
+    // let response = MessageResponse { status, message };
+    HttpResponse::Ok().body(message)
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct MessageResponse {
+    pub status: String,
+    pub message: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Build You Own Load Balancer");
 }
